ca4b941e3b3e31998cd4ed54bf1d49aa5d9f7be1
zhurunovrodion <zhurunov.rodion@yandex.ru>
2017-08-25 21:21:24 +0500

    показывание / скрытие главного меню

--- js/main.js
+++ js/main.js
@@ -2,4 +2,30 @@ $(document).ready(initialize);
 
 function initialize(){
 	initializeMap();
+
+	addMainMenuButtonHandlers();
+	//('navigationMenu__menuLine__menu__mainMenu__content')
+}
+
+function addMainMenuButtonHandlers(){
+	$('.navigationMenu__menuLine__menu__mainMenu').on('click', onMainMenuButtonClick)
+}
+
+function onMainMenuButtonClick(e){
+	var $menuButton = $(e.currentTarget);
+	var $menuElement = $('.navigationMenu__menuLine__menu__mainMenu__content');	
+
+	if($menuButton.is('.state-closed')){
+		$menuButton.removeClass('state-closed');
+		$menuButton.addClass('state-opened');
+		
+		$menuElement.removeClass('state-closed');
+		$menuElement.addClass('state-opened');
+	}else{
+		$menuButton.addClass('state-closed');
+		$menuButton.removeClass('state-opened');
+
+		$menuElement.addClass('state-closed');
+		$menuElement.removeClass('state-opened');
+	}
 }
